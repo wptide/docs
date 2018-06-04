d047f2babb6d90e163f30decf6ed4e01a07280a2
Bartosz Makos <loudnownet@gmail.com>
2018-06-04 16:19:47 +0200

    add api check component

--- docs/hello.md
+++ docs/hello.md
@@ -17,17 +17,17 @@
   <canvas class="hero__canvas" id="canvas"></canvas>
 </div>
 
-<div class="api-check">
+<div class="api-check" data-api-check>
 	<div class="api-check__title">Search Plugins & Themes</div>
 	<form class="api-check__form" action="#">
-		<input type="radio" id="api-check__radio--plugins" name="api-check__radio" class="api-check__radio" checked>
+		<input type="radio" id="api-check__radio--plugins" name="api-check__radio" class="api-check__radio" value="plugin" checked>
 		<label class="api-check__label" for="api-check__radio--plugins">
 			<svg class="api-check__icon" width="20" height="20" xmlns="http://www.w3.org/2000/svg">
 				<path d="M14 2.7L9.8 6.9 7.4 4.4 11.6.2c.4-.4 1.3-.2 2 .5.7.6.8 1.5.4 2zM3.6 5l1.2-1.5 11.7 11.7-1.6 1.1c-.9 1-3.4 1.5-5 1.5H5l-1.6 1.6a2 2 0 0 1-2.7 0 2 2 0 0 1 0-2.7L2.2 15v-5c0-1.5.5-4.2 1.4-5.1zm9.4 5l4.2-4c.5-.4 1.4-.3 2 .4.7.7 1 1.6.5 2l-4.3 4.3L13 10z"/>
 			</svg>
 			Plugins
 		</label>
-		<input type="radio" id="api-check__radio--themes" name="api-check__radio" class="api-check__radio">
+		<input type="radio" id="api-check__radio--themes" name="api-check__radio" value="theme" class="api-check__radio">
 		<label class="api-check__label" for="api-check__radio--themes">
 			<svg class="api-check__icon" width="25" height="21" xmlns="http://www.w3.org/2000/svg">
 				<path d="M17.6 11.8L8.4 2.6l2-2c.6-.7 3-.6 4.5.4 1.6 1 1.9 1.7 3.8 2.8 1.5.8 3.2 1.6 5.8 1l-7 7zm-1 .9L7.5 3.5 5.1 5.8c-.5.5-.5 1.3 0 1.8L6.5 9c.5.5.5 1.4 0 1.9L3.6 13l-1.3 1c-1.7 2-3 4.4-1.8 5.7 1.3 1.3 3.7 0 5.5-1.8l1-1.3 2.3-2.9c.5-.5 1.3-.5 1.8 0l1.4 1.4c.5.5 1.3.5 1.8 0l2.4-2.3z" />
@@ -44,7 +44,6 @@
 		</div>
 	</form>
 </div>
-
 
 # Hello
 
--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,10 +1,101 @@
+// Polyfills
+// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
+
 // Check API component
+function ApiCheck (el) {
+  var form = el.querySelector('form')
+  var input = el.querySelector('input[type=text]')
+
+  form.addEventListener('submit', handleSubmit)
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    var option = form.querySelector('input[type=radio]:checked')
+    if (option && input.value) {
+      fetchApi(option.value, input.value)
+    }
+  }
+
+  function handleClose (e) {
+    e.preventDefault()
+    removeResponse()
+  }
+
+  function showResponse (msg) {
+    removeResponse()
+    var pre = document.createElement('pre')
+    var closeLink = document.createElement('a')
+    pre.classList.add('api-check__pre')
+    closeLink.setAttribute('href', '#')
+    closeLink.classList.add('api-check__close')
+    closeLink.innerText = 'Close'
+    el.appendChild(pre).innerHTML = syntaxHighlight(msg)
+    pre.appendChild(closeLink)
+    closeLink.addEventListener('click', handleClose)
+  }
+  
+  function removeResponse () {
+    var previousPre = el.querySelector('pre')
+    if (previousPre) previousPre.remove()
+  }
+
+  function fetchApi (type, name, cb) {
+    var req = new XMLHttpRequest()
+    req.open('GET', 'https://wptide.org/api/tide/v1/audit/wporg/'+ type +'/'+ name, true)
+    req.onreadystatechange = function () {
+      if (req.readyState == 4) {
+        if(req.status == 200) showResponse(JSON.parse(req.responseText))
+        else showResponse('The entered API endpoint is invalid\n')
+      }
+    }
+    req.send(null)
+  }
+
+  function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+      json = JSON.stringify(json, undefined, 2);
+    }
+
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+      var cls = 'hljs-number';
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = 'hljs-attr';
+        } else {
+          cls = 'pl-s';
+        }
+      } else if (/true|false/.test(match)) {
+        cls = 'pl-c1';
+      } else if (/null/.test(match)) {
+        cls = 'pl-c1';
+      }
+      return '<span class="' + cls + '">' + match + '</span>';
+    });
+  }
+}
 
 // Waves for homepage hero section
 
 function Waves (canvas) {
   var ctx = canvas.getContext('2d')
-  var parentRect = canvas.parentNode.getBoundingClientRect()
   var height = canvas.height = window.innerHeight
   var width = canvas.width = window.innerWidth - 400
 
@@ -12,8 +103,8 @@ function Waves (canvas) {
   var fov = 1024
   var zRows = width / 60
   var xRows = width / 50
-  var spacing = width / xRows
   var yBase = 800
+  var spacing = width / xRows
   var dotSize = 2
   var tickDiv = 20
   var tick = 0
@@ -86,16 +177,21 @@ function Waves (canvas) {
 
 document.addEventListener('DOMContentLoaded', function() {
   var canvas = document.getElementById('canvas')
+  var apiCheck = document.querySelector('[data-api-check]')
   var waves
 
   function createWaves() {
     if (waves) waves.stop()
-    if (matchMedia('(min-width: 959px)').matches) {
+    if (matchMedia('(min-width: 959px)').matches && canvas) {
       waves = new Waves(canvas)
       waves.run()
     }
   }
 
-  window.addEventListener('load', createWaves)
+  window.addEventListener('load', function() {
+    createWaves()
+    if (apiCheck) new ApiCheck(apiCheck)
+  })
+
   window.addEventListener('resize', createWaves)
 })
