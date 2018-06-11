c5ce4e6c33065147fcee9f111ec0e8bfbca2ca57
Bartosz Makos <loudnownet@gmail.com>
2018-06-11 13:51:49 +0200

    remove routing file

--- functions.php
+++ functions.php
@@ -4,6 +4,5 @@
 define('DOCS_PATH', get_stylesheet_directory_uri() . '/_docpress');
 
 // Require theme functions
-require_once( 'functions/routing.php' );
 require_once( 'functions/enqueue-styles.php' );
 require_once( 'functions/enqueue-scripts.php' );
