2125e12c164b2f275dd28ce2349119d927e18f38
Bartosz Makos <loudnownet@gmail.com>
2018-06-12 17:14:52 +0200

    custom header 200 when file is found

--- index.php
+++ index.php
@@ -3,13 +3,22 @@
  * The main template file
  */
 
-header('HTTP/1.1 200 OK');
+$file = $_SERVER[REQUEST_URI] != '/' ? $_SERVER[REQUEST_URI] : '/index';
+$file = substr($file, -1) == '/' ? $file . 'index' : $file;
+$file = preg_replace('#\?.+#i', '', $file);
+$file = dirname(__FILE__) . '/_docpress' . $file . '.html';
+
+if (file_exists($file)) {
+	header('HTTP/1.1 200 OK');
+}
+
 get_header();
 
-	$file = $_SERVER[REQUEST_URI] != '/' ? $_SERVER[REQUEST_URI] : '/index';
-	$file = substr($file, -1) == '/' ? $file . 'index' : $file;
-	$file = preg_replace('#\?.+#i', '', $file);
+	if (file_exists($file)) {
+		include($file);
+		
+	} else {
 
-	include('_docpress' . $file . '.html');
+	}
 
 get_footer();
