2d56566eafe53d001b1b4ef5477c0ed4afde13c1
Derek Herman <derek@valendesigns.com>
2018-06-13 13:33:35 +0200

    Add quotes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * The main template file
  */
 
-$file = $_SERVER[REQUEST_URI] != '/' ? $_SERVER[REQUEST_URI] : '/index';
+$file = $_SERVER['REQUEST_URI'] != '/' ? $_SERVER['REQUEST_URI'] : '/index';
 $file = substr($file, -1) == '/' ? $file . 'index' : $file;
 $file = preg_replace('#\?.+#i', '', $file);
 $file = dirname(__FILE__) . '/_docpress' . $file . '.html';
