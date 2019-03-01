4607e3eda5b51e4bc4f9d27c7e9ae9a194590829
Derek Herman <derek@valendesigns.com>
2019-02-28 22:31:25 -0800

    Add report meta to columns for testing

--- functions.php
+++ functions.php
@@ -20,7 +20,9 @@ define( 'DOCS_FILE_PATH', docpress_get_file_path() );
 
 // Remove unused scripts & styles.
 add_action('init', function() {
-	if ( ! is_admin() ) {
+	global $pagenow;
+
+	if ( 'wp-login.php' !== $pagenow && ! is_admin() ) {
 		wp_deregister_script( 'wp-embed' );
 		wp_deregister_script( 'jquery' );
 		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
@@ -111,3 +113,84 @@ add_action( 'wp_head', function() {
 
 	<?php }
 }, 0 );
+
+// Add a column to the edit post list
+add_filter( 'manage_edit-audit_columns', 'add_new_columns' );
+/**
+ * Add new columns to the post table
+ *
+ * @param Array $columns - Current columns on the list post
+ */
+function add_new_columns( $columns ) {
+ 	$column_meta = array( 'meta' => 'Report Meta' );
+	$columns = array_slice( $columns, 0, 2, true ) + $column_meta + array_slice( $columns, 2, NULL, true );
+	return $columns;
+}
+
+/**
+ * Add the column scripts and styles.
+ */
+add_filter( 'admin_footer', function() { ?>
+<style>
+.toggle-meta {
+	cursor: pointer;
+}
+</style>
+<script>
+(function( $ ){
+	$( '.report-meta' ).prepend( '<a class="toggle-meta">Show</a>' );
+	$( '.report-meta' ).children( 'pre' ).hide();
+
+	$( '.toggle-meta' ).on( 'click', function( e ){
+		e.preventDefault();
+
+		if ( 'Hide' === $( this ).text() ) {
+			$( this ).text( 'Show' );
+			$( this ).next('pre').hide();
+		} else {
+			$(this).text( 'Hide' );
+			$( this ).next('pre').show();
+		}
+	} );
+} )( jQuery );
+</script>
+<?php }, 999 );
+
+// Add action to the manage post column to display the data
+add_action( 'manage_audit_posts_custom_column' , 'custom_columns' );
+/**
+ * Display data in new columns
+ *
+ * @param  $column Current column
+ *
+ * @return Data for the column
+ */
+function custom_columns( $column ) {
+	global $post;
+	switch ( $column ) {
+		case 'meta':
+			$data = array();
+
+			$meta = get_post_meta( $post->ID, '_audit_phpcs_wordpress', true );
+			if ( isset( $meta['raw'] ) ) {
+				$data['phpcs_wordpress'] = $meta['raw'];
+			}
+
+			$meta = get_post_meta( $post->ID, '_audit_phpcs_phpcompatibility', true );
+			if ( isset( $meta['raw'] ) ) {
+				$data['phpcs_phpcompatibility'] = $meta['raw'];
+			}
+
+			$meta = get_post_meta( $post->ID, '_audit_lighthouse', true );
+			if ( isset( $meta['raw'] ) ) {
+				$data['lighthouse'] = $meta['raw'];
+			}
+
+			if ( $data ) { ?>
+				<div class="report-meta">
+					<pre><?php echo json_encode( $data, JSON_PRETTY_PRINT ); ?></pre>
+				</div>
+			<?php }
+		break;
+	}
+}
