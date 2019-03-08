367bfcfe30c23b5a7cda9b8c8f2a5a2af1e4a3ca
Derek Herman <derek@valendesigns.com>
2019-03-08 15:32:48 -0800

    Update GA tracking code

--- functions.php
+++ functions.php
@@ -103,12 +103,12 @@ add_action( 'wp_head', function() {
 	if ( strpos( get_home_url(), 'wptide.org' ) !== false ) { ?>
 
 <!-- Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135375588-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135952573-1"></script>
 <script>
 	window.dataLayer = window.dataLayer || [];
 	function gtag(){dataLayer.push(arguments);}
 	gtag('js', new Date());
-	gtag('config', 'UA-135375588-1');
+	gtag('config', 'UA-135952573-1');
 </script>
 
 	<?php }
