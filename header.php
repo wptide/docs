5fbacdaa69fcf25b80f82bf780dc3689b29bccbb
Fahim Murshed <themeumfahim@gmail.com>
2020-02-25 16:54:25 +0600

    Fix doc typo and contributor added

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the header
+ * The template for displaying the header.
  *
  * Displays all of the head element and everything up until the "page" div.
  * 
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The main template file
+ * The main template file.
  */
 
 get_header();
